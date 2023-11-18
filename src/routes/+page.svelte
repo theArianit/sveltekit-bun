fa2bc09092a79977b0d7a3148634f5a9097c84e4
Arianit <17601489+theArianit@users.noreply.github.com>
2023-11-18 21:00:24 +0100

    dev script and welcome page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to SvelteKit with bun</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Also take a look at <a href="https://bun.sh/guides/ecosystem/sveltekit">Build an app with SvelteKit and Bun.</a></p>
